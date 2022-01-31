835d62362f6d643d966359906abfdaf540961611
Ang Jun Ray <angjunray@live.com>
2022-01-31 14:44:25 +0800

    Setup :)

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # School End Timer
- 
---
+++ src/Components/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const colors = [
+    ["072ac8","1360e2","1e96fc","60b6fb","a2d6f9","cfe57d","fcf300","fedd00","ffc600","ffcb17"]
+  ];
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
